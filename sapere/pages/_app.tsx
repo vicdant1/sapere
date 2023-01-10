32a1c1d96cb887e09694dfdd91601f37eede9ad6
vicdant1 <dantjv@outlook.com>
2023-01-10 20:11:45 -0300

    working with material ui

--- sapere/pages/_app.tsx
+++ sapere/pages/_app.tsx
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { orange } from '@mui/material/colors'
+
+
+const theme = createTheme({
+  palette:{
+    primary: {
+      main: "#CCC"
+    },
+    secondary: {
+      main: orange[400]
+    }
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  (
+      <>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
+    )
 }

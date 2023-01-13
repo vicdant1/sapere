b2908217edfa9157367f2054acbe93064091c6b1
vicdant1 <dantjv@outlook.com>
2023-01-13 09:03:24 -0300

    adding topbar - working on it

---
+++ mui_sapere/components/global/Topbar/Topbar.tsx
@@ -0,0 +1,47 @@
+import { DarkModeOutlined, LightModeOutlined, NotificationsOutlined, PersonOutlined, Search, SettingsOutlined } from "@mui/icons-material";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../../theme";
+
+
+export const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <Search />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutlined />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
--- mui_sapere/pages/_app.tsx
+++ mui_sapere/pages/_app.tsx
@@ -1,27 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { orange } from '@mui/material/colors';
-import { CssBaseline } from '@mui/material';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: orange[500]
-    }
-  }
-});
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { ColorModeContext, useMode } from "../theme";
+import { Topbar } from "../components/global/Topbar/Topbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, colorMode] = useMode();
 
-  return(
+  return (
     <>
-      <CssBaseline/>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>      
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <main className="content">
+              <Topbar />
+              <Component {...pageProps} />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
